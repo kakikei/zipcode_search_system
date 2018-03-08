9e0353a674e7bb515bb8f168f471d1afd1e8a605
keisuke.kakinuma <keisuke.kakinuma@RM0040.local>
2018-03-08 13:04:00 +0900

    ボタンを押してエラー排出

--- js/load.js
+++ js/load.js
@@ -1,16 +1,20 @@
 function setState(){
   var zipcode = $('#zipcode').val();
   $.ajax({
-    timeout  : 1000,
+    timeout  : 10000,
     url      : 'http://zipcloud.ibsnet.co.jp/api/search?zipcode=',
     datetype : 'json',
-    type     : 'get'
+    type     : 'GET',
     date     : {
-      address   : zipcode,
-      language : 'ja',
-    },
-    then.()
+      address  : zipcode,
+      language : 'ja'
+    }
   })
-}
-var url = 'http://zipcloud.ibsnet.co.jp/api/search?zipcode=' + user_postadresss;//リクエストURL
-console.log(url);
+  .then(function(res){//データ取得
+    console.log(zipcode);
+    $('#state')  .val(res.results[0]['address1']); //都道府県
+    $('#city')   .val(res.results[0]['address2']); //市区町村
+    $('#address').val(res.results[0]['address3']); //番地
+    return false;
+  },//データ不取得
+)};
